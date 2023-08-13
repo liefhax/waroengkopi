e07d4187608cb93bfdba381da6ce264b183e71c0
alief <122020553+liefhax@users.noreply.github.com>
2023-08-13 14:03:48 +0700

    Delete javascript directory

--- javascript/js/js.js
+++
@@ -1,5 +0,0 @@
-// for loop
-
-for(let i = 1; i <= 10; i =1 + 1) {
-    console.log('ALIEF')
-}
--- javascript/js/main.js
+++
@@ -1,4 +0,0 @@
-let namaGuru = ['jodi', 'bunga']
-namaGuru.push('nama', 'maman')
-
-alert(namaGuru[1])
